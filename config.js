d2ccecdbc9e83694274bcb73ae6c73a3426f37eb
bobo-lion <76873444+bobo-lion@users.noreply.github.com>
2024-03-12 17:15:22 +0100

    waariupdate
    waarilive app

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  //Server database
+  MONGODB_USERNAME: "jahbobolion",
+  MONGODB_PASSWORD: "Terremerejah92",
+  MONGODB_DB_NAME: "waarilive",
+
+  //Port
+  PORT: process.env.PORT || 5000,
+
+  //Secret key for API
+  SECRET_KEY: "fc/8d:?D_uE`8un",
+
+  //Secret key for jwt
+  JWT_SECRET: "rQ(eUJMl*}v]ke+",
+
+  //Gmail credentials for send email
+  EMAIL: "jahbobolion@gmail.com",
+  PASSWORD: "grhm rykz gjyd enle",
+
+  //Server URL
+  baseURL: "https://baaraso.com/earn5",
+
+  //aws credentials
+  endpoint: "your_endpoint",
+  aws_access_key_id: "your_access_key",
+  aws_secret_access_key: "your_secret_key",
+  region: "your_region",
+  bucketName: "your_bucketName",
+
+  //Firebase server key for notification
+  SERVER_KEY: "AAAAcZJ0Yog:APA91bGcJ4yuTMnXEtJTHRLveVy13KbE3YH47JK4GJOOyVStbkgfdbdr8FWc8GUwppTYr4dnGUKjl0GbY3OuI8a2BgxIFQdOgz0EBA9azuh10Pp4kWdvCp1TS5Ojk55NYxmslGJfV8d6",
+};
